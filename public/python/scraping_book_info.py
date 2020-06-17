d1bff0b46c8a5cf339128d9541462732dc293d7d
Hasu <syu8.0324@outlook.jp>
2020-06-17 15:19:40 +0900

    edited

--- public/python/scraping_book_info.py
+++ public/python/scraping_book_info.py
@@ -4,33 +4,17 @@ import urllib.request
 import csv
 import datetime
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-# Seleniumをあらゆる環境で起動させるChromeオプション
-options = Options()
-options.add_argument('--disable-gpu');
-options.add_argument('--disable-extensions');
-options.add_argument('--proxy-server="direct://"');
-options.add_argument('--proxy-bypass-list=*');
-options.add_argument('--start-maximized');
-options.add_argument('--headless'); # ※ヘッドレスモードを使用する場合、コメントアウトを外す
 
-DRIVER_PATH = '{{/usr/local/bin/chromedriver}}'
-
-driver = webdriver.Chrome(executable_path = DRIVER_PATH, chrome_options = options)
+driver = webdriver.Firefox()
 
 #webページアクセス
 url = 'https://books.rakuten.co.jp/calendar/001001/weekly/?tid=2020-05-18'
+
 driver.get(url)
 
 html = driver.page_source
-soup = beautifulsoup(html,"html.parser")
+
+soup = BeautifulSoup(html,"html.parser")
 
 bk_title = [i.get_text() for i in  soup.select("[class='item-title__text']")]
 date = [i.get_text() for i in soup.select("[class='item-release__date']")]
@@ -43,6 +27,7 @@ print(len(bk_title))
 print(len(date))
 print(len(link))
 
+driver.quit()
 
 #for i in range(len(bk_title)):
 #    title_list.append([bk_title[i].text])
@@ -62,7 +47,7 @@ print(len(link))
 #write_date_csv.writerows(date_list)
 #write_link_csv.writerows(link_list)
 
-f.close()
+#f.close()
 #page_li = soup.select("[class='rbcomp__pager'] li")
 #print(page_li)
 ## page_len = [sub.select('a') for sub in soup.select("[class='rbcomp__page'] li")]
